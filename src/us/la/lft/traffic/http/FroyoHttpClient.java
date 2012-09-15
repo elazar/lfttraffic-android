6a9538474cac139d5bbcc37cbd24d54ccb65de5d
elazar <tobias382@gmail.com>
2012-09-14 20:30:17 -0500

    Refactored to remove usage of AndroidHttpClient, which appears to cause issues with decoding downloaded image files

--- src/us/la/lft/traffic/http/FroyoHttpClient.java
+++ src/us/la/lft/traffic/http/FroyoHttpClient.java
@@ -1,15 +1,38 @@
 package us.la.lft.traffic.http;
 
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.HttpResponse;
 
-public class FroyoHttpClient extends BaseHttpClient {
-    public HttpClient getHttpClient() {
-        try {
-            return (HttpClient) Class.forName("android.net.http.AndroidHttpClient")
-                .getMethod("newInstance", String.class)
-                .invoke(null, "Android " + android.os.Build.VERSION.RELEASE);
-        } catch (Exception e) {
-            return null;
-        }
-    }
+import java.io.InputStream;
+import java.io.IOException;
+
+public class FroyoHttpClient implements HttpGetRequest {
+	public InputStream get(String urlString) {
+		BasicHttpParams httpParams = new BasicHttpParams();
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+		ClientConnectionManager connectionManager = new ThreadSafeClientConnManager(httpParams, schemeRegistry);
+        DefaultHttpClient httpClient = new DefaultHttpClient(connectionManager, httpParams);
+		HttpGet httpRequest = new HttpGet(urlString);
+		HttpResponse httpResponse;
+		BufferedHttpEntity httpEntity;
+		try {
+			httpResponse = httpClient.execute(httpRequest);
+			if (httpResponse.getStatusLine().getStatusCode() != 200) {
+				return null;
+			}
+			httpEntity = new BufferedHttpEntity(httpResponse.getEntity());
+			return httpEntity.getContent();
+		} catch (IOException e) {
+			return null;
+		}
+	}
 }
--- src/us/la/lft/traffic/http/HttpGetRequestFactory.java
+++ src/us/la/lft/traffic/http/HttpGetRequestFactory.java
@@ -4,13 +4,6 @@ import android.os.Build;
 
 public class HttpGetRequestFactory {
     public static HttpGetRequest getHttpGetRequest() {
-        int currentVersion = Build.VERSION.SDK_INT;
-        int froyoVersion = 8;
-        if (currentVersion < froyoVersion) {
-            return new PreFroyoHttpClient();
-        } else if (currentVersion == froyoVersion) {
-            return new FroyoHttpClient();
-        }
-        return new PostFroyoHttpClient();
+        return (Build.VERSION.SDK_INT <= 8) ? new FroyoHttpClient() : new PostFroyoHttpClient();
     }
 }
