667ab5319eeab50efb96f9b41898024d9684518e
elazar <tobias382@gmail.com>
2012-09-29 17:53:29 -0500

    Added gzip support to FroyoHttpClient

--- src/us/la/lft/traffic/http/FroyoHttpClient.java
+++ src/us/la/lft/traffic/http/FroyoHttpClient.java
@@ -1,5 +1,11 @@
 package us.la.lft.traffic.http;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.PlainSocketFactory;
@@ -9,12 +15,9 @@ import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
-import org.apache.http.HttpResponse;
-
-import java.io.InputStream;
-import java.io.IOException;
 
 public class FroyoHttpClient implements HttpGetRequest {
+	@Override
 	public InputStream get(String urlString) {
 		BasicHttpParams httpParams = new BasicHttpParams();
 		SchemeRegistry schemeRegistry = new SchemeRegistry();
@@ -24,13 +27,21 @@ public class FroyoHttpClient implements HttpGetRequest {
 		HttpGet httpRequest = new HttpGet(urlString);
 		HttpResponse httpResponse;
 		BufferedHttpEntity httpEntity;
+		Header contentEncoding;
+		InputStream inputStream;
 		try {
+			httpRequest.addHeader("Accept-Encoding", "gzip");
 			httpResponse = httpClient.execute(httpRequest);
 			if (httpResponse.getStatusLine().getStatusCode() != 200) {
 				return null;
 			}
 			httpEntity = new BufferedHttpEntity(httpResponse.getEntity());
-			return httpEntity.getContent();
+			inputStream = httpEntity.getContent();
+			contentEncoding = httpResponse.getFirstHeader("Content-Encoding");
+			if (contentEncoding != null && contentEncoding.getValue().equalsIgnoreCase("gzip")) {
+				inputStream = new GZIPInputStream(inputStream);
+			}
+			return inputStream;
 		} catch (IOException e) {
 			return null;
 		}
